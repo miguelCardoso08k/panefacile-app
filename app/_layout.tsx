754f64818d320887e1ca4b8fe6f281fff945167d
miguel-cardoso08k <joaomiguel.cardoso2006@outlook.com>
2026-09-19 17:25:33 -0300

    feat: load Inter font family

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,34 @@
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-
-import "../global.css";
 import { MockDataProvider } from "@/src/data/mock-store";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import "../global.css";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
     <>
       <StatusBar style="dark" />
